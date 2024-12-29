e5cf52160173cd72d77a79f54028a037db90da9c
anant <anant@20AHARI-NARAIN>
2024-12-29 10:38:39 +0000

    Moved button class to StaticItems.cs

--- MazeT/StaticItems.cs
+++ MazeT/StaticItems.cs
@@ -113,4 +113,23 @@ namespace MazeT
             }
         }
     }
+
+    /// <summary>
+    /// These are rectangular buttons that will be used for the UI.
+    /// </summary>
+    internal class Button : StandardSprite2D
+    {
+        public Button(Point position, Texture2D background)
+        {
+            //Background of button likely to be same for all buttons
+            image = background;
+            rect = image.Bounds;
+            rect.Location = position;
+        }
+
+        public bool IsMouseOnButton(Point mouse_position)
+        {
+            return rect.Contains(mouse_position);
+        }
+    }
 }
--- MazeT/UI.cs
+++ MazeT/UI.cs
@@ -8,40 +8,5 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace MazeT
 {
-    /// <summary>
-    /// These are rectangular buttons that will be used for
-    /// the UI.
-    /// </summary>
-    internal class Button: StandardSprite2D
-    {
-        public Button(Point position, Texture2D background)
-        {
-            //Background of button likely to be same for all buttons
-            image = background;
-            rect = image.Bounds;
-            rect.Location = position;
-        }
-
-        public bool IsMouseOnButton(Point mouse_position)
-        {
-            return rect.Contains(mouse_position);
-        }
-    }
-
-    ///// <summary>
-    ///// This class will be used to represent the power up box, 
-    ///// which is used on one screen
-    ///// </summary>
-    //internal class PowerUpBox
-    //{
-    //    public Button button;
-    //    public string description;
-    //    public bool is_alive; //Determines if the power up box has been used or not.
-    //    public int type; //Correspond
-
-    //    public void Display(SpriteBatch sprite_batch)
-    //    {
-    //        button.Display(sprite_batch);
-    //    }
-    //}
+    
 }
